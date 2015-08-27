8225b31254ee0b01a471a72593963e4e5efb450c
Brian <blau08@gmail.com>
2015-08-27 09:12:37 -0700

    working

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require "pry"
 get("/") do
   erb(:index)
 end
-ActiveRecord::Base.establish_connection(ENV['postgres://yfnigfwranpprr:KUvhh49qrOnYNOtv6NqG_X7x8-@ec2-23-23-188-252.compute-1.amazonaws.com:5432/d90dhghk30gusn'])
+# ActiveRecord::Base.establish_connection(ENV['postgres://yfnigfwranpprr:KUvhh49qrOnYNOtv6NqG_X7x8-@ec2-23-23-188-252.compute-1.amazonaws.com:5432/d90dhghk30gusn'])
 
 get("/recipes") do
   @recipes = Recipe.order('rating DESC')
@@ -29,8 +29,8 @@ end
 get("/recipe/:id") do
   recipe_id = params.fetch("id").to_i
   @recipe = Recipe.find(recipe_id)
+  @category = Category.all()
   @ingredients = Ingredient.all()
-  @categories = Category.all()
   erb(:recipe)
 end
 
--- lib/recipe.rb
+++ lib/recipe.rb
@@ -2,6 +2,6 @@ class Recipe < ActiveRecord::Base
   validates(:name, :presence => true)
   validates :name, uniqueness: true
   has_and_belongs_to_many(:ingredients)
-  has_and_belongs_to_many(:catagories)
+  has_and_belongs_to_many(:categories)
 
 end
